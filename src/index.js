0c049ff34fd4f01d0792e3752f04145057937fde
mohammed-misbah <115168697+mohammed-misbah@users.noreply.github.com>
2024-01-19 21:21:02 +0530

    route updated

--- src/index.js
+++ src/index.js
@@ -12,12 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider store={store}>
-    <App/>
-    </Provider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
 
 reportWebVitals();
