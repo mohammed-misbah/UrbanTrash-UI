b066436f4f700672acd69325a68a00f761c1f631
mohammed-misbah <115168697+mohammed-misbah@users.noreply.github.com>
2024-01-22 22:19:21 +0530

    baseUrl updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = "https://13.53.131.202/";
+export const baseUrl = "https://api.firefitnes.online/";
 
 const user = localStorage.getItem('user')
 export const addressList = (user) => 'api/listAddress/user';
